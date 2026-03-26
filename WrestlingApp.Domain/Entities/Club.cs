9a67a597fd01f1bfb9fb359dfd2d6603436bd7c9
Seyfi Isayev <isayevseyfi3277@gmail.com>
2026-03-26 14:46:59 +0400

    feat: implement Club and Wrestler entities in Domain layer

---
+++ WrestlingApp.Domain/Entities/Club.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrestlingApp.Domain.Entities
+{
+    public class Club
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public virtual ICollection<Wrestler> Wrestlers { get; set; } = new List<Wrestler>();
+    }
+}
