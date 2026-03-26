b1fbadf71d3383e6f25f324c14ff8744a62ade8c
Seyfi Isayev <isayevseyfi3277@gmail.com>
2026-03-26 14:35:43 +0400

    removed useless classes

--- WrestlingApp.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WrestlingApp.Application
-{
-    public class Class1
-    {
-
-    }
-}
--- WrestlingApp.Infrastructure/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WrestlingApp.Infrastructure
-{
-    public class Class1
-    {
-
-    }
-}
