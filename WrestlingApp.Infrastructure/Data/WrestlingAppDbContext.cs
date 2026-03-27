b83ad2609789f0f585bca99e660355fe42ae5360
Seyfi Isayev <isayevseyfi3277@gmail.com>
2026-03-27 07:55:30 +0400

    feat: finalize infrastructure setup and align project dependencies

---
+++ WrestlingApp.Infrastructure/Data/WrestlingAppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WrestlingApp.Domain.Entities;
+
+namespace WrestlingApp.Infrastructure.Data
+{
+    public class WrestlingAppDbContext : DbContext
+    {
+        public WrestlingAppDbContext(DbContextOptions<WrestlingAppDbContext> options) : base(options) { }
+
+        public DbSet<Club> Clubs { get; set; }  
+        public DbSet<Wrestler> Wrestlers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Club>()
+                .HasMany(c => c.Wrestlers)
+                .WithOne(w => w.Club)
+                .HasForeignKey(w => w.ClubId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
