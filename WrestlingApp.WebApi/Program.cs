62cfe28a49b4799da705397936ff741e104142ec
Seyfi Isayev <isayevseyfi3277@gmail.com>
2026-03-28 08:23:38 +0400

    fix: ensure repository files are correctly saved and tracked

--- WrestlingApp.WebApi/Program.cs
+++ WrestlingApp.WebApi/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using WrestlingApp.Application.Interfaces;
 using WrestlingApp.Infrastructure.Data;
+using WrestlingApp.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<WrestlingAppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,7 +21,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.>
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
